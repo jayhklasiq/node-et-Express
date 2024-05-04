736513987c16ebc67fde7edf218c14266195ef11
Joshua Olaoye <113859966+jayhklasiq@users.noreply.github.com>
2024-05-04 03:08:17 +0000

    updating from replit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,20 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
-console.log("Hello World");
+app.get("/json", function(req, res) {
+  res.json({
+    message: "Hello json"
+  })
+})
 
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(3500, () => {
+  console.log('Server is running on http://localhost:3500');
+});
 
 module.exports = app;
